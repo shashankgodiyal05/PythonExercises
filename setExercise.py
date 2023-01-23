8039f52466da59336e047878b4585b2962e8b4df
Shashank Godiyal <42241879+shashankgodiyal05@users.noreply.github.com>
2023-01-23 21:02:49 +0530

    Add files via upload

---
+++ setExercise.py
@@ -0,0 +1,67 @@
+# 1. Write a python program to remove an item from a set if it is present in the set.
+
+
+# st2 = {1, 2, 3, 4, 6, 34, 9}
+
+# for x in st2:
+#     if x == 6:
+#         st2.remove(x)
+
+# print(st2)
+
+
+
+# 2. Write a python program to find maximum and the minimum value in a set.
+
+
+# st2 = {1, 2, 3, 4, 6, 34, 9}
+
+# print(max(st2))
+# print(min(st2))
+
+
+
+# 3. Write a python program to check if two given sets have no elements in common.
+
+
+# st3_1 = {1, 2, 3, 4, 6, 34, 9}
+# st3_2 = {5, 7, 11, 10, 13}
+# count = 0
+
+# for x in st3_1:
+#     for y in st3_2:
+#         if y == x:
+#             count = 1
+
+# print(st3_1)
+# print(st3_2)
+
+# if count == 1:
+#     print("Given two sets do have elements in common.")
+
+# else:
+#     print("No, given two sets dont have elements in common.")
+
+
+
+# 4. Write a python program to remove the intersection of a 2nd set from the 1st set.
+
+
+# st4_1 = {1, 2, 3, 4, 6, 34, 9}
+# st4_2 = {5, 7, 9, 11, 10, 13}
+
+# print(st4_1.difference(st4_2))
+# print(st4_2)
+
+
+
+# 5. Write a python program to find the elements in a given set that are not in another set.
+
+
+# st5_1 = {1, 2, 3, 4, 6, 34, 9}
+# st5_2 = {5, 7, 9, 11, 10, 13}
+
+# print(st5_1.difference(st5_2))
+# print(st5_2)
+
+
